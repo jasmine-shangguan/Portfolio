ef0d91e275c4f7bdc20fc6adc11186df008687c7
Jasmine Shangguan <yingzi.shangguan@gmail.com>
2025-04-30 13:09:50 -0400

    change hero content text

--- src/components/sub/HeroContent.tsx
+++ src/components/sub/HeroContent.tsx
@@ -37,7 +37,7 @@ const HeroContent = () => {
         >          
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
           <h1 className='Welcome-text text-[17px] mr-[10px]'>
-              Ex Software Developer | Aspiring Data Scientist
+              Aspiring Data Scientist | Ex-Software Developer
           </h1>
         </motion.div>
         <motion.div
